eda20fb70dc68a3f3a3c88f27a5ac2fc9acd5d06
julian <julianhahn01@gmail.com>
2020-07-20 18:08:33 +0200

    erfolgreich ein low poly chess board erstellt und eine color pallette als material benutzt

---
+++ Assets/Scripts/WASD_Movement.cs
@@ -0,0 +1,29 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WASD_Movement : MonoBehaviour
+{
+    [Range(0.0f, 5.0f)]
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = new Vector3(-1*Input.GetAxis("Horizontal"), 0, -1*Input.GetAxis("Vertical"));
+        moveCharacter(direction);
+    }
+
+    void moveCharacter(Vector3 direction)
+    {
+        transform.Translate(direction * speed * Time.deltaTime);
+    }
+}
+
+
